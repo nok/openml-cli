f15275f04e2b333923c5110c259e78f07fcd519c
Darius Morawiec <nok@users.noreply.github.com>
2018-11-30 02:40:42 +0100

    release/0.1.0: Add config parser

--- openml_cli/cli/__main__.py
+++ openml_cli/cli/__main__.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, print_function, unicode_literals
-
-import sys
 import os
-from os.path import abspath, expanduser, join
+import sys
 from argparse import ArgumentParser, SUPPRESS, RawTextHelpFormatter, _HelpAction
 from dotenv import load_dotenv, find_dotenv
 
@@ -13,14 +10,14 @@ from openml_cli.cli import *
 from openml_cli.utils import dedent
 
 
-def add_arg_help(p):
+def _add_arg_help(p):
     p.add_argument(
         '-h', '--help',
         action=_HelpAction,
         help="Show this help message and exit.")
 
 
-def add_arg_version(p):
+def _add_arg_version(p):
     p.add_argument(
         '-v', '--version',
         action='version',
@@ -28,11 +25,35 @@ def add_arg_version(p):
         help='Show the version number and exit.')
 
 
-def add_arg_debug(p):
+def _add_arg_debug(p):
     p.add_argument(
         '--debug',
         action='store_true',
         help=SUPPRESS)
+
+
+def _configure_parser_dataset(p):
+    sp = p.add_parser(
+        'dataset',
+        description='dataset description',
+        help='dataset help',
+        epilog='dataset epilog',
+        add_help=False,
+    )
+    _add_arg_help(sp)
+    sp.set_defaults(func=main_dataset.execute)
+
+
+def _configure_parser_config(p):
+    sp = p.add_parser(
+        'config',
+        description='config description',
+        help='config help',
+        epilog='config epilog',
+        add_help=False,
+    )
+    _add_arg_help(sp)
+    sp.set_defaults(func=main_config.execute)
 
 
 def parse_args(args):
@@ -47,9 +68,9 @@ def parse_args(args):
         epilog=epilog,
         formatter_class=RawTextHelpFormatter,
         add_help=False)
-    add_arg_help(p)
-    add_arg_version(p)
-    add_arg_debug(p)
+    _add_arg_help(p)
+    _add_arg_version(p)
+    _add_arg_debug(p)
 
     # Setup subparsers:
     sp = p.add_subparsers(
@@ -57,66 +78,56 @@ def parse_args(args):
         dest='cmd',
     )
     sp.required = True
-    configure_parser_dataset(sp)
-    configure_parser_config(sp)
+    _configure_parser_dataset(sp)
+    _configure_parser_config(sp)
 
     # Show help by default:
     if len(sys.argv) == 1:
         p.print_help(sys.stderr)
         sys.exit(1)
 
-    # Call the subparser:
-    args = p.parse_args(args)
-    func = args.func
-    delattr(args, 'func')
-    func(args)
+    return p.parse_args(args)
 
 
-def configure_parser_dataset(p):
-    sp = p.add_parser(
-        'dataset',
-        description='dataset description',
-        help='dataset help',
-        epilog='dataset epilog',
-        add_help=False,
-    )
-    add_arg_help(sp)
-    sp.set_defaults(func=main_dataset.execute)
+def parse_config():
+    # https://openml.github.io/OpenML/Client-API-Standards/
+    cfg_file_path = os.path.expanduser('~/.openml/config')
 
+    # Default config:
+    config = {
+        'server': 'https://www.openml.org',
+        'verbosity': '0'
+    }
 
-def configure_parser_config(p):
-    sp = p.add_parser(
-        'config',
-        description='config description',
-        help='config help',
-        epilog='config epilog',
-        add_help=False,
-    )
-    add_arg_help(sp)
-    sp.set_defaults(func=main_config.execute)
+    # Write config:
+    if not os.path.exists(cfg_file_path):
+        os.makedirs(os.path.expanduser('~/.openml'))
+        with open(cfg_file_path, 'wt') as fh:
+            for key, value in config.items():
+                line = '{}=[]\n'.format(key, value)
+                fh.write(str(line))
+        return config
+
+    # Read config:
+    with open(cfg_file_path, 'rt') as fh:
+        for line in fh:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                key_value = line.split('=')
+                if len(key_value) == 2:
+                    key = key_value[0].strip().strip('"')
+                    value = key_value[1].strip().strip('"')
+                    config[key] = value
+    return config
 
 
 def main():
-
-    # Config? JSON?
-    config_file = os.path.expanduser('~/.openml/config')
-    if not os.path.exists(config_file):
-        os.makedirs(os.path.expanduser('~/.openml'), exist_ok=True)
-        with open(config_file, 'wt') as fh:
-            fh.write('apikey')
-    else:
-        props = {}
-        with open(config_file, "rt") as fh:
-            for line in fh:
-                l = line.strip()
-                if l and not l.startswith('#'):
-                    key_value = l.split('=')
-                    if len(key_value) == 2:
-                        key = key_value[0].strip().strip('"')
-                        value = key_value[1].strip().strip('"')
-                        props[key] = value
-
-    parse_args(sys.argv[1:])
+    args = parse_args(sys.argv[1:])
+    config = parse_config()
+    if hasattr(args, 'func'):
+        func = args.func
+        delattr(args, 'func')
+        func(config, args)
 
 
 if __name__ == "__main__":
--- openml_cli/utils/__init__.py
+++ openml_cli/utils/__init__.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, print_function, unicode_literals
-
-# import six
+import six
 from textwrap import dedent as textwrap_dedent
 
 
 def dedent(text):
-    return textwrap_dedent(text)
-    # return six.b(textwrap_dedent(text))
+    return six.b(textwrap_dedent(text))
