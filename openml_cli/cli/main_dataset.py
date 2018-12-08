f5a896cafaddfce4ef74719acb1627fbfccb5aea
Darius Morawiec <nok@users.noreply.github.com>
2018-12-08 09:54:19 +0100

    release/0.1.0: Replace camel_to_snake to utils

--- openml_cli/cli/main_dataset.py
+++ openml_cli/cli/main_dataset.py
@@ -198,9 +198,3 @@ def main(config, args):
             print('Download completed ... {}'.format(filepath))
 
         sys.stdout.flush()
-
-
-def _camel_to_snake(name):
-    # https://stackoverflow.com/a/1176023/1293700
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
--- openml_cli/utils/__init__.py
+++ openml_cli/utils/__init__.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import re
 import six
 from textwrap import dedent as textwrap_dedent
 
 
 def dedent(text):
     return six.b(textwrap_dedent(text))
+
+
+def camel_to_snake(text):
+    # https://stackoverflow.com/a/1176023/1293700
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
